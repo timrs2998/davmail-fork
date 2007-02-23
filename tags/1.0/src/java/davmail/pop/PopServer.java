f927832c5fe3037af8cb75f899d70e38e97f8e6e
mguessan <mguessan@3d1905a2-6b24-0410-a738-b14d5a86fcbd>
2007-02-23 13:47:39 +0000

    First release on sourceforge, complete with installer

---
+++ tags/1.0/src/java/davmail/pop/PopServer.java
@@ -0,0 +1,26 @@
+package davmail.pop;
+
+
+import davmail.AbstractServer;
+
+import java.net.Socket;
+
+/**
+ * Pop3 server
+ */
+public class PopServer extends AbstractServer {
+    public final static int DEFAULT_PORT = 110;
+
+    /**
+     * Create a ServerSocket to listen for connections.
+     * Start the thread.
+     */
+    public PopServer(int port) {
+        super((port == 0) ? PopServer.DEFAULT_PORT : port);
+    }
+
+    public void createConnectionHandler(Socket clientSocket) {
+        new PopConnection(clientSocket);
+    }
+
+}
