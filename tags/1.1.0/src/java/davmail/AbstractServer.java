c95a4593ac0f1c0c639a72c78714e26a9760a797
mguessan <mguessan@3d1905a2-6b24-0410-a738-b14d5a86fcbd>
2007-11-16 16:21:30 +0000

    1.1.0 release

---
+++ tags/1.1.0/src/java/davmail/AbstractServer.java
@@ -0,0 +1,87 @@
+package davmail;
+
+import davmail.tray.DavGatewayTray;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Generic abstract server common to SMTP and POP3 implementations
+ */
+public abstract class AbstractServer extends Thread {
+    private int port;
+    private ServerSocket serverSocket;
+
+    /**
+     * Create a ServerSocket to listen for connections.
+     * Start the thread.
+     */
+    public AbstractServer(int port, int defaultPort) throws IOException {
+        if (port == 0) {
+            this.port = defaultPort;
+        } else {
+            this.port = port;
+        }
+        //noinspection SocketOpenedButNotSafelyClosed
+        serverSocket = new ServerSocket(port);
+    }
+
+
+    /**
+     * The body of the server thread.  Loop forever, listening for and
+     * accepting connections from clients.  For each connection,
+     * create a Connection object to handle communication through the
+     * new Socket.
+     */
+    public void run() {
+        Socket clientSocket = null;
+        AbstractConnection connection = null;
+        try {
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                clientSocket = serverSocket.accept();
+                DavGatewayTray.debug("Connection from " + clientSocket.getInetAddress() + " on port " + port);
+                // only accept localhost connections for security reasons
+                if (Settings.getBooleanProperty("davmail.allowRemote") || 
+                        clientSocket.getInetAddress().toString().indexOf("127.0.0.1") > 0) {
+                    connection = createConnectionHandler(clientSocket);
+                    connection.start();
+                } else {
+                    clientSocket.close();
+                    DavGatewayTray.warn("Connection from external client refused");
+                }
+                System.gc();
+            }
+        } catch (IOException e) {
+            // do not warn if exception on socket close (gateway restart)
+            if (!serverSocket.isClosed()) {
+                DavGatewayTray.warn("Exception while listening for connections", e);
+            }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                DavGatewayTray.warn("Exception closing client socket", e);
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    public abstract AbstractConnection createConnectionHandler(Socket clientSocket);
+
+    public void close() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            DavGatewayTray.warn("Exception closing server socket", e);
+        }
+    }
+}
+
---
+++ tags/1.1.0/src/test/davmail/exchange/TestExchangeSession.java
@@ -0,0 +1,41 @@
+package davmail.exchange;
+
+import davmail.Settings;
+import org.apache.commons.httpclient.util.URIUtil;
+
+/**
+ *
+ */
+public class TestExchangeSession {
+
+    protected TestExchangeSession() {
+    }
+
+    public static void main(String[] argv) {
+        int currentArg = 0;
+        Settings.setConfigFilePath(argv[currentArg++]);
+        Settings.load();
+
+        ExchangeSession session = new ExchangeSession();
+        // test auth
+        try {
+            ExchangeSession.checkConfig();
+            session.login(argv[currentArg++], argv[currentArg++]);
+
+            ExchangeSession.Folder folder = session.selectFolder(argv[currentArg++]);
+            String messageName;
+            messageName = URIUtil.decode(argv[currentArg]);
+
+            long startTime = System.currentTimeMillis();
+            ExchangeSession.Message messageTest = session.getMessage(folder.folderUrl + "/" + messageName);
+            System.out.println("******");
+            messageTest.write(System.out);
+            System.out.println("Elapsed time " + (System.currentTimeMillis() - startTime) + " ms");
+
+            session.purgeOldestTrashMessages();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
