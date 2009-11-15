d79cfa282a99a69db7a07f2270c1a2dd46765465
mguessan <mguessan@3d1905a2-6b24-0410-a738-b14d5a86fcbd>
2009-11-15 23:47:11 +0000

    3.6.0 release

---
+++ tags/3.6.0/src/test/davmail/util/StringUtilTest.java
@@ -0,0 +1,47 @@
+/*
+ * DavMail POP/IMAP/SMTP/CalDav/LDAP Exchange Gateway
+ * Copyright (C) 2009  Mickael Guessant
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package davmail.util;
+
+import junit.framework.TestCase;
+
+/**
+ * Test StringUtil.
+ */
+public class StringUtilTest extends TestCase {
+    /**
+     * Test get token
+     */
+    public void testGetToken() {
+        assertNull(StringUtil.getToken(null, null, null));
+        assertNull(StringUtil.getToken(null, "\'", "\'"));
+        assertNull(StringUtil.getToken("'test", "\'", "\'"));
+        assertEquals("test", StringUtil.getToken("'test'", "'", "'"));
+        assertEquals("test", StringUtil.getToken("value=\"test\"", "value=\"", "\""));
+    }
+
+    /**
+     * Test replace token
+     */
+    public void testReplaceToken() {
+        assertNull(StringUtil.replaceToken(null, null, null, null));
+        assertNull(StringUtil.replaceToken(null, null, null, "new"));
+        assertEquals("'new'", StringUtil.replaceToken("'old'", "'", "'", "new"));
+        assertEquals("value=\"new\"", StringUtil.replaceToken("value=\"old\"", "value=\"", "\"", "new"));
+    }
+}
