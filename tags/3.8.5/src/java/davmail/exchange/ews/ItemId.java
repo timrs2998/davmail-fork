94530d42d3d97862640f95abacb32d76f43bfcb0
mguessan <mguessan@3d1905a2-6b24-0410-a738-b14d5a86fcbd>
2010-09-27 21:40:11 +0000

    3.8.5 release

---
+++ tags/3.8.5/src/java/davmail/exchange/ews/ItemId.java
@@ -0,0 +1,66 @@
+/*
+ * DavMail POP/IMAP/SMTP/CalDav/LDAP Exchange Gateway
+ * Copyright (C) 2010  Mickael Guessant
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package davmail.exchange.ews;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Item id.
+ */
+public class ItemId {
+    protected final String id;
+    protected final String changeKey;
+
+    /**
+     * Build Item id from response item.
+     *
+     * @param item response item
+     */
+    public ItemId(EWSMethod.Item item) {
+        this.id = item.get("ItemId");
+        this.changeKey = item.get("ChangeKey");
+    }
+
+    /**
+     * Build Item id object from item id.
+     *
+     * @param itemId item id
+     */
+    public ItemId(String itemId) {
+        this.id = itemId;
+        this.changeKey = null;
+    }
+
+    /**
+     * Write item id as XML.
+     *
+     * @param writer request writer
+     * @throws IOException on error
+     */
+    public void write(Writer writer) throws IOException {
+        writer.write("<t:ItemId Id=\"");
+        writer.write(id);
+        if (changeKey != null) {
+            writer.write("\" ChangeKey=\"");
+            writer.write(changeKey);
+        }
+        writer.write("\"/>");
+    }
+}
